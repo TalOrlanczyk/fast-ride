7206f572b88149ee0810c51d4c428b3ea83a511a
TalOrlanczyk <tal.orlanczyk@gmail.com>
2020-10-20 18:41:15 +0300

    Updates

---
+++ precache-manifest.9de78d719b78dfaa746e244729336166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cab0eff690cc6ddb939a79f23b1fb39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1438aa1ba9d45f28117a",
+    "url": "/static/css/main.0fa3821c.chunk.css"
+  },
+  {
+    "revision": "b41af5b19f36e6454506",
+    "url": "/static/js/2.2d8e8da4.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.2d8e8da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1438aa1ba9d45f28117a",
+    "url": "/static/js/main.9930170c.chunk.js"
+  },
+  {
+    "revision": "18e4f150b8eccb89262e",
+    "url": "/static/js/runtime-main.cd0d320b.js"
+  }
+]);
