efa75a30d17c501611d3b4ce915a4317b0c70999
TalOrlanczyk <tal.orlanczyk@gmail.com>
2020-10-20 18:25:19 +0300

    add padding to make all the text be in the center

--- src/Components/Card/Title/Title.js
+++ src/Components/Card/Title/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ title }) => {
   return (
     <div className="card-title">
-      <h1 className="text-white text-center">{title}</h1>
+      <h1 className="text-white text-center p1">{title}</h1>
     </div>
   );
 };
